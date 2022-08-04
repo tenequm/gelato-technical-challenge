5d7ed55f79304f1d7a24f349b5dc79ce92b379f4
Misha Kolesnik <tenequm@gmail.com>
2022-08-04 22:24:18 +0300

    Add initial application Helm chart structure

--- app/server.js
+++ app/server.js
@@ -6,18 +6,24 @@ const express = require("express");
 const PORT = 8080;
 const HOST = "0.0.0.0";
 
+const environment = process.env.ENVIRONMENT || "local";
 const blockchain_network_name = process.env.BLOCKCHAIN_NETWORK_NAME || "goerli";
 const rpc_endpoint = process.env.RPC_ENDPOINT || "http://localhost:8080";
-const db_host = process.env.DB_HOST || "http://localhost:5432";
-const environment = process.env.ENVIRONMENT || "local";
+
+// DB configs
+const db_host = process.env.DB_HOST || "localhost";
+const db_user = process.env.DB_USER || "local-user";
+const db_password = process.env.DB_PASSWORD || "local-password";
 
 // App
 const app = express();
 app.get("/", (req, res) => {
-  res.send(`Blockchain network name: ${blockchain_network_name}
+  res.send(`<br>Environment: ${environment}
+    <br>Blockchain network name: ${blockchain_network_name}
     <br>RPC Endpoint: ${rpc_endpoint}
     <br>Database Host: ${db_host}
-    <br>Environment: ${environment}
+    <br>Database User: ${db_user}
+    <br>Database Password: ${db_password}
   `);
 });
 
